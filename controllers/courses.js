7017c52c311d7ba4d3669689332c2996fc98050a
Noyan Alimov <noyan.alimov@gmail.com>
2020-05-05 18:19:32 +0100

    started working with a new get request in controllers

--- controllers/courses.js
+++ controllers/courses.js
@@ -7,18 +7,37 @@ const Course = require('../models/Course');
 // @route   GET /api/v1/bootcamps/:bootcampId/courses
 // @access  Public
 exports.getCourses = asyncHandler(async (req, res, next) => {
-    let query;
+  let query;
 
-    if (req.params.bootcampId) {
-        query = Course.find({ bootcamp: req.params.bootcampId });
-    } else {
-        query = Course.find().populate({
-            path: 'bootcamp',
-            select: 'name description'
-        });
-    }
+  if (req.params.bootcampId) {
+    query = Course.find({ bootcamp: req.params.bootcampId });
+  } else {
+    query = Course.find().populate({
+      path: 'bootcamp',
+      select: 'name description'
+    });
+  }
 
-    const courses = await query;
+  const courses = await query;
 
-    res.status(200).json({ success: true, count: courses.length, data: courses });
+  res.status(200).json({ success: true, count: courses.length, data: courses });
+});
+
+// @desc    Get a single course
+// @route   GET /api/v1/courses/:id
+// @access  Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: 'bootcamp',
+    select: 'name description'
+  });
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`No course with the id of ${req.params.id}`),
+      404
+    );
+  }
+
+  res.status(200).json({ success: true, data: course });
 });
