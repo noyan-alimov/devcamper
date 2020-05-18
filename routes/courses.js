6e43f27a76deb33245210d5fbdc6cf393441de58
Noyan Alimov <noyan.alimov@gmail.com>
2020-05-18 20:43:55 +0100

    implemented cookies, added autherization middleware to protect and authorize

--- routes/courses.js
+++ routes/courses.js
@@ -10,6 +10,8 @@ const {
 const Course = require('../models/Course');
 const advancedResults = require('../middleware/advancedResults');
 
+const { protect, authorize } = require('../middleware/auth');
+
 const router = express.Router({ mergeParams: true });
 
 router
@@ -21,7 +23,11 @@ router
     }),
     getCourses
   )
-  .post(addCourse);
-router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
+  .post(protect, authorize('publisher', 'admin'), addCourse);
+router
+  .route('/:id')
+  .get(getCourse)
+  .put(protect, authorize('publisher', 'admin'), updateCourse)
+  .delete(protect, authorize('publisher', 'admin'), deleteCourse);
 
 module.exports = router;
